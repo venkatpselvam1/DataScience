25bd48b179d6b789eaf7938a0b5fd6aae1cfc1f9
venkatpselvam1 <venkatpselvam1@gmail.com>
2021-05-14 12:44:04 +0530

    Update quick_visualization.py

--- 005_DataVisualization/quick_visualization.py
+++ 005_DataVisualization/quick_visualization.py
@@ -204,5 +204,13 @@ Additional
 
 plt.scatter(sales, profit, s=50, alpha=0.7) # here alpha indicates the transperency
 plt.plot(months, sales, marker="X")         # mark the point as X
+================================================================================================================
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+list_1 = [48.49, 67.54, 57.47, 68.17, 51.18, 68.31, 50.33, 66.7, 45.62, 43.59, 53.64, 70.08, 47.69, 61.27, 44.14, 51.62, 48.72, 65.11]
+plt.hist(list_1, bins=4, range=[40,80])
+plt.show()
 
 ================================================================================================================
