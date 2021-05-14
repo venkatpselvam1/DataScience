a8b954e1d2271dad4590dcc1c9a9030d04e25da6
venkatpselvam1 <venkatpselvam1@gmail.com>
2021-05-14 11:21:19 +0530

    Update quick_visualization.py

--- 005_DataVisualization/quick_visualization.py
+++ 005_DataVisualization/quick_visualization.py
@@ -56,3 +56,25 @@ plt.ylabel("Sales", fontdict={'fontsize': 12, 'fontweight' : 5, 'color' : 'Brown
 plt.legend()
 plt.show()
 ================================================================================================================
+Line chart
+================================================================================================================
+import numpy as np
+import matplotlib.pyplot as plt
+
+months = np.array(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'])
+sales = np.array([241268.56, 184837.36, 263100.77, 242771.86, 288401.05, 401814.06, 258705.68, 456619.94, 481157.24, 422766.63, 555279.03, 503143.69])
+
+plt.plot(months, sales)
+plt.title("Sales for the year", fontdict={"fontweight":5, "fontsize":20, "color":"green"})
+plt.xlabel("month", fontdict={"fontweight":5, "fontsize":15, "color":"red"})
+plt.ylabel("sales", fontdict={"fontweight":5, "fontsize":15, "color":"red"})
+
+ticks = np.arange(0,600000,50000)
+ticks_label = ["{}k".format(x/1000) for x in ticks]
+plt.yticks(ticks, ticks_label)
+plt.xticks(rotation=45)
+
+for xy in zip(months, sales):
+    plt.annotate(text="{:.0f}k".format(xy[1]/1000),xy=xy, textcoords="data")
+plt.show()
+================================================================================================================
