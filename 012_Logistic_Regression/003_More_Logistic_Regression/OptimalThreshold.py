27e6ea74f547435e0780e3c3e62bfd23750319a7
venkatpselvam1 <venkatpselvam1@gmail.com>
2021-09-05 13:18:56 +0530

    Add files via upload

---
+++ 012_Logistic_Regression/003_More_Logistic_Regression/OptimalThreshold.py
@@ -0,0 +1,19 @@
+# Now let's calculate accuracy sensitivity and specificity for various probability cutoffs.
+cutoff_df = pd.DataFrame( columns = ['prob','accuracy','sensi','speci'])
+from sklearn.metrics import confusion_matrix
+
+# TP = confusion[1,1] # true positive 
+# TN = confusion[0,0] # true negatives
+# FP = confusion[0,1] # false positives
+# FN = confusion[1,0] # false negatives
+
+num = [0.0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+for i in num:
+    cm1 = metrics.confusion_matrix(y_train_pred_final.Churn, y_train_pred_final[i] )
+    total1=sum(sum(cm1))
+    accuracy = (cm1[0,0]+cm1[1,1])/total1
+    
+    speci = cm1[0,0]/(cm1[0,0]+cm1[0,1])
+    sensi = cm1[1,1]/(cm1[1,0]+cm1[1,1])
+    cutoff_df.loc[i] =[ i ,accuracy,sensi,speci]
+print(cutoff_df)
